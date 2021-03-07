466b1b4ac0c8b3950c354db6b9279a47d371a9e8
Valentin Waevoda <valentin3840@gmail.com>
2021-03-07 16:12:50 +0300

    start_set v1.3.8

--- src/js/components/app.component.js
+++ src/js/components/app.component.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import HomePageComponent from './pages/homePage.component';
-import GalleryPageComponent from "../components/pages/galleryPage.component";
-import NoMatchPageComponent from "../components/pages/noMatch.component"
+import {HomePageComponent} from './pages/homePage.component';
+import {GalleryPageComponent} from "../components/pages/galleryPage.component";
+import {NoMatchPageComponent} from "../components/pages/noMatch.component";
+import HeaderComponent from "../components/layout/header.component";
+import {FooterComponent} from "../components/layout/footer.component";
 
 
 
@@ -11,14 +13,13 @@ export const AppComponent = props =>{
     
         return(    
                 <Router>
-                    
+                        <HeaderComponent/>
                                <Switch>
                                 <Route exact path="/" component = {HomePageComponent} />
                                 <Route path="/gallery" component = {GalleryPageComponent} />
                                 <Route path="*" component = {NoMatchPageComponent} />    
                             </Switch> 
-                            
-                    
+                        <FooterComponent/>          
                 </Router>
         )
     
--- src/js/components/layout/header.component.js
+++ src/js/components/layout/header.component.js
@@ -1,85 +1,13 @@
-import { AppBar, IconButton, Toolbar, Typography, Hidden, Paper } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import {makeStyles} from "@material-ui/core";
 import React from 'react';
-import { withRouter } from 'react-router-dom';
-import  DrawerMenu  from './drawer/drawer.component';
+import {withRouter} from "react-router-dom"
 
-
-const useStyle = makeStyles(theme=>( {
-    appBar: {
-        top: 'auto',
-        bottom: 0,
-    },
-     
-    text: {
-        padding: theme.spacing(2),
-    },
-}))
 
 const HeaderComponent = (props) => {
-    const [isOpenMenuLeft, setisOpenMenuLeft] = React.useState(false);
-    const [isOpenMenuBottom, setisOpenMenuBottom] = React.useState(false);
-
-
-    const classes = useStyle();
-    const toggleMenuLeft = () => {
-        setisOpenMenuLeft(!isOpenMenuLeft);
-    }
-    const toggleMenuBottom = () => {
-        setisOpenMenuBottom(!isOpenMenuBottom);
-    }
-    
+   
     
         return (
             <>
-            <Paper square>
-            <Hidden implementation="css" mdUp >
-                <Paper square elevation={1}>
-                    <Typography className={classes.text} variant="h6" gutterBottom>
-                    {   props.location.pathname == "/"
-                        ?"home".toUpperCase()
-                        : props.location.pathname.slice(1).toUpperCase()}
-                    </Typography>
-                </Paper>
-            </Hidden>
-            <Hidden implementation="css" smDown>
-                <AppBar position="sticky">
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleMenuLeft} >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6">
-                            { props.location.pathname == "/"
-                            ?"home".toUpperCase()
-                            : props.location.pathname.slice(1).toUpperCase()}
-                        </Typography>
-                        
-                        
-                    </Toolbar>
-                </AppBar>
-            </Hidden>
-
-            <Hidden implementation="css" mdUp>
-                <AppBar position="fixed" className={classes.appBar} >
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleMenuBottom} >
-                            <MenuIcon />
-                        </IconButton>
-                        <Typography variant="h6">
-                            { props.location.pathname == "/"
-                            ?"home".toUpperCase()
-                            : props.location.pathname.slice(1).toUpperCase()}
-                        </Typography>
-                        
-                        
-                    </Toolbar>
-                </AppBar>                
-            </Hidden>
-            </Paper>
-            
-            <DrawerMenu isOpenMenuBottom={isOpenMenuBottom} isOpenMenuLeft={isOpenMenuLeft} toggleMenuBottom={toggleMenuBottom} toggleMenuLeft={toggleMenuLeft} />
-                     
+                <p>Header is here</p>
             </>
             
         )
